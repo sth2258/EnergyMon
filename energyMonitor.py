4d6fe8b77b05f93599d8213612198d30c0199f61
sth2258 <sth2258@users.noreply.github.com>
2019-09-10 21:55:51 -0400

    Allows multiple SMS numbers to be specified in config, checks againsts 2 previous values of power

--- energyMonitor.py
+++ energyMonitor.py
@@ -32,32 +32,40 @@ for device in manager.outlets:
     ret = device.displayJSON()
    
     currentValue = float(ret['Power'])
+    previousValue1 = float(ret['Power'])
+    previousValue2 = float(ret['Power'])
+    
     
     if(os.path.exists(ret['Device Name']+".txt") == False):
         print("File does not exist - creating with initial value.")
         f = open(ret['Device Name']+".txt", "w")
-        f.write(str(currentValue))
+        f.write(str(currentValue)+"-"+str(currentValue))
         f.close()
-        previousValue = currentValue
+        previousValue1 = currentValue
+        previousValue2 = currentValue
     else:
+        #First, read operation to get the previous two values
         f = open(ret['Device Name']+".txt", "r")
-        previousValue = f.readline()
-
-        previousValue = float(previousValue)
+        fileContents = f.readline()
+        splitLine = fileContents.split("-")
+        previousValue1 = float(splitLine[0])
+        previousValue2 = float(splitLine[1])
         f.close()
+        
+        #Second, write operation to write current plus the newer of the previous values (previousValue1)
         f = open(ret['Device Name']+".txt", "w")
-        print(str(datetime.datetime.now())+" " + ret['Device Name']+": Current = "+ str(currentValue) + ", Previous = "+str(previousValue))
-        f.write(str(currentValue))
+        print(str(datetime.datetime.now())+" " + ret['Device Name']+": Current = "+ str(currentValue) + ", Previous1 = "+str(previousValue1)+ ", Previous2 = "+str(previousValue2))
+        f.write(str(currentValue)+"-"+str(previousValue1))
         f.close()
     
-    if(previousValue<POWERTHRESHOLD and currentValue>POWERTHRESHOLD):
-        print("Machine appears to have started")
-        client.publish(PhoneNumber=config['DEFAULT']['PhoneNumber1'],Message=(config['DEFAULT']['StartMessage']).replace("plug",ret['Device Name']))
-        client.publish(PhoneNumber=config['DEFAULT']['PhoneNumber2'],Message=(config['DEFAULT']['StartMessage']).replace("plug",ret['Device Name']))
-    elif(previousValue>POWERTHRESHOLD and currentValue<POWERTHRESHOLD):
-        print("Machine appears to have stopped")
-        client.publish(PhoneNumber=config['DEFAULT']['PhoneNumber1'],Message=(config['DEFAULT']['CompleteMessage']).replace("plug",ret['Device Name']))
-        client.publish(PhoneNumber=config['DEFAULT']['PhoneNumber2'],Message=(config['DEFAULT']['CompleteMessage']).replace("plug",ret['Device Name']))
-    elif(previousValue<POWERTHRESHOLD and currentValue<POWERTHRESHOLD):
-        print("Machine appears idle")
+    if(previousValue1<POWERTHRESHOLD and previousValue2<POWERTHRESHOLD and currentValue>POWERTHRESHOLD):
+        print(str(datetime.datetime.now())+" " +ret['Device Name']+" appears to have started")
+        for number in config['DEFAULT']['PhoneNumber'].split(","):
+            client.publish(PhoneNumber=number,Message=(config['DEFAULT']['StartMessage']).replace("plug",ret['Device Name']))
+    elif(previousValue2>POWERTHRESHOLD and previousValue1<POWERTHRESHOLD and currentValue<POWERTHRESHOLD):
+        print(str(datetime.datetime.now())+" " +ret['Device Name']+" appears to have stopped")
+        for number in config['DEFAULT']['PhoneNumber'].split(","):
+            client.publish(PhoneNumber=number,Message=(config['DEFAULT']['StartMessage']).replace("plug",ret['Device Name']))
+    else:
+        print(str(datetime.datetime.now())+" " +ret['Device Name']+" appears idle")
         

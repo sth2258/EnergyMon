c639da5a5aac5ef6f34cd5ffa06f082f9651861f
sth2258 <sth2258@users.noreply.github.com>
2019-11-23 20:01:23 -0500

    Update energyMonitor.py
    Filter functionality based on names of outlets in ini file

--- energyMonitor.py
+++ energyMonitor.py
@@ -21,6 +21,7 @@ InfluxDBIP = str(config['DEFAULT']['InfluxDBIP'])
 UserName = config['DEFAULT']['UserName']
 Password = config['DEFAULT']['Password']
 botoclient = boto3.client("sns",aws_access_key_id=config['DEFAULT']['aws_access_key_id'],aws_secret_access_key=config['DEFAULT']['aws_secret_access_key'],region_name=config['DEFAULT']['region_name'])
+OutletsToProcess = config['DEFAULT']['OutletsToProcess'].split(",")
 
 sys.path.insert(0, VESYNCAPIPATH)
 from pyvesync import VeSync
@@ -37,66 +38,67 @@ client = InfluxDBClient(InfluxDBIP, 8086, '', '', 'EnergyMonitor')
 # Display outlet device information
 for device in manager.outlets:
     ret = device.displayJSON()
-
-    currentValue = float(ret['Power'])
-    
-    json_body = [
-        {
-            "measurement": "Wattage",
-            "tags": {
-                "device": ret['Device Name']
-            },
-            "time": datetime.utcnow(),
-            "fields": {
-                "value": currentValue
+    print("Checking if array contians " + ret['Device Name'])
+    if(ret['Device Name'] in OutletsToProcess):
+        currentValue = float(ret['Power'])
+        
+        json_body = [
+            {
+                "measurement": "Wattage",
+                "tags": {
+                    "device": ret['Device Name']
+                },
+                "time": datetime.utcnow(),
+                "fields": {
+                    "value": currentValue
+                }
             }
-        }
-    ]
-    client.write_points(json_body)
-    print(str(datetime.utcnow())+" " + ret['Device Name']+": Current = "+ str(currentValue))
+        ]
+        client.write_points(json_body)
+        print(str(datetime.utcnow())+" " + ret['Device Name']+": Current = "+ str(currentValue))
 
-    #Get the older values from the DB (if they even exist)
-    PreviousQueryDates = (datetime.utcnow() - timedelta(minutes=6))
-    q1 = 'SELECT "value" FROM "EnergyMonitor"."autogen"."Wattage" WHERE time > ' + "'"+ str(PreviousQueryDates)+"' and device='"+ret['Device Name']+"'"
-    #Test Values
-    #PreviousQueryDates = (datetime(2019,9,23,15,8,6) - timedelta(minutes=6))
-    #q1 = 'SELECT "value" FROM "EnergyMonitor"."autogen"."Wattage" WHERE time > ' + "'"+ str(PreviousQueryDates)+"' and time <= '"+ str(datetime(2019,9,23,15,8,10)) +"'and device='"+ret['Device Name']+"'"
-    
-    res = client.query(q1)
-	
-    Counter=0
-    PreviousValuesAboveThreshold=False
-    CountOfPointsAboveThreshold=0
-    CountOfPointsBelowThreshold=0
-    print(ret['Device Name']+":")
-    col = []
-    
-    for point in res.get_points():
-        col.append(point['value'])
-        print("\t" + str(point['time'])+": Value = "+ str(point["value"]))
+        #Get the older values from the DB (if they even exist)
+        PreviousQueryDates = (datetime.utcnow() - timedelta(minutes=6))
+        q1 = 'SELECT "value" FROM "EnergyMonitor"."autogen"."Wattage" WHERE time > ' + "'"+ str(PreviousQueryDates)+"' and device='"+ret['Device Name']+"'"
+        #Test Values
+        #PreviousQueryDates = (datetime(2019,9,23,15,8,6) - timedelta(minutes=6))
+        #q1 = 'SELECT "value" FROM "EnergyMonitor"."autogen"."Wattage" WHERE time > ' + "'"+ str(PreviousQueryDates)+"' and time <= '"+ str(datetime(2019,9,23,15,8,10)) +"'and device='"+ret['Device Name']+"'"
+        
+        res = client.query(q1)
+        
+        Counter=0
+        PreviousValuesAboveThreshold=False
+        CountOfPointsAboveThreshold=0
+        CountOfPointsBelowThreshold=0
+        print(ret['Device Name']+":")
+        col = []
+        
+        for point in res.get_points():
+            col.append(point['value'])
+            print("\t" + str(point['time'])+": Value = "+ str(point["value"]))
 
-        if(point["value"] >  POWERTHRESHOLD):
-            PreviousValuesAboveThreshold = True
-            CountOfPointsAboveThreshold = CountOfPointsAboveThreshold + 1
-        else:
-            PreviousValuesAboveThreshold = False
-            CountOfPointsBelowThreshold = CountOfPointsBelowThreshold + 1
-        Counter = Counter+1
-    
-    print("\t"+str(col))
-    size = len(col)
-    if(size > 5):
-        if((col[size-1] < POWERTHRESHOLD and col[size-2] < POWERTHRESHOLD and col[size-3] < POWERTHRESHOLD and col[size-4] < POWERTHRESHOLD and col[size-5] < POWERTHRESHOLD) and (col[0] > POWERTHRESHOLD or col[1] > POWERTHRESHOLD or col[2] > POWERTHRESHOLD or col[3] > POWERTHRESHOLD)):
-            print("Current values are below, and former values were above. Safe to say it stopped")
-            for number in config['DEFAULT']['PhoneNumber'].split(","):
-                botoclient.publish(PhoneNumber=number,Message=(config['DEFAULT']['CompleteMessage']).replace("plug",ret['Device Name']))
-    
-    
-    
-    print("\n\t**Count Above Thresold: "+str(CountOfPointsAboveThreshold))
-    print("\t**Count Below Thresold: "+str(CountOfPointsBelowThreshold))
-    print(str(Counter) + " previous values found in the past 5 minutes")
+            if(point["value"] >  POWERTHRESHOLD):
+                PreviousValuesAboveThreshold = True
+                CountOfPointsAboveThreshold = CountOfPointsAboveThreshold + 1
+            else:
+                PreviousValuesAboveThreshold = False
+                CountOfPointsBelowThreshold = CountOfPointsBelowThreshold + 1
+            Counter = Counter+1
+        
+        print("\t"+str(col))
+        size = len(col)
+        if(size > 5):
+            if((col[size-1] < POWERTHRESHOLD and col[size-2] < POWERTHRESHOLD and col[size-3] < POWERTHRESHOLD and col[size-4] < POWERTHRESHOLD and col[size-5] < POWERTHRESHOLD) and (col[0] > POWERTHRESHOLD or col[1] > POWERTHRESHOLD or col[2] > POWERTHRESHOLD or col[3] > POWERTHRESHOLD)):
+                print("Current values are below, and former values were above. Safe to say it stopped")
+                for number in config['DEFAULT']['PhoneNumber'].split(","):
+                    botoclient.publish(PhoneNumber=number,Message=(config['DEFAULT']['CompleteMessage']).replace("plug",ret['Device Name']))
+        
+        
+        
+        print("\n\t**Count Above Thresold: "+str(CountOfPointsAboveThreshold))
+        print("\t**Count Below Thresold: "+str(CountOfPointsBelowThreshold))
+        print(str(Counter) + " previous values found in the past 5 minutes")
 
-    if(PreviousValuesAboveThreshold == True and currentValue < POWERTHRESHOLD and CountOfPointsBelowThreshold > 3):
-        print("Machine is confirmed off for the past 5 minutes worth of checks")
-	
+        if(PreviousValuesAboveThreshold == True and currentValue < POWERTHRESHOLD and CountOfPointsBelowThreshold > 3):
+            print("Machine is confirmed off for the past 5 minutes worth of checks")
+        
